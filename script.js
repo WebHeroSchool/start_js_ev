4a6655007c08838795aecf2385da6fb63578fb9b
Владимирова Екатерина <echapyrkina@mail.ru>
2020-09-20 12:35:02 +0400

    changes in script

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('Go for it!');
+fetch('https://api.github.com/users/6thSence')
+  .then(result => result.json())
+    .then(data => {
+      const gitHubUser = document.createElement('a');
+      gitHubUser.href = data.html_url;
+      gitHubUser.innerHTML = data.html_url;
+      const gitHubUser_name = document.createElement('h1');
+      gitHubUser_name.title = data.name;
+      gitHubUser_name.innerHTML = data.name;
+      const gitHubUser_avatar = document.createElement('img');
+      gitHubUser_avatar.src = data.avatar_url;
+      const gitHubUser_bio = document.createElement('p');
+      gitHubUser_bio.innerHTML = data.bio;
+      document.body.append(gitHubUser);
+      document.body.append(gitHubUser_name);
+      document.body.append(gitHubUser_avatar);
+      document.body.append(gitHubUser_bio);
+  })
+  .catch(error => console.log('Информация о пользователе недоступна'));
